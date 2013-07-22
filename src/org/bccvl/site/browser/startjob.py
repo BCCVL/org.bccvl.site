93d1fe9459354af70cc988dd4bd86629ea4fdae4
Gerhard Weis <gerhard.weis@gmail.com>
2013-07-22 14:28:29 +1000

    manually submit jobs without quota

--- src/org/bccvl/site/browser/startjob.py
+++ src/org/bccvl/site/browser/startjob.py
@@ -3,6 +3,7 @@ from org.bccvl.compute.bioclim import execute
 from plone.app.uuid.utils import uuidToObject
 from zope.component import getUtility
 from plone.app.async.interfaces import IAsyncService
+from plone.app.async import service
 from Products.statusmessages.interfaces import IStatusMessage
 
 
@@ -16,10 +17,18 @@ class StartJobView(BrowserView):
         env = uuidToObject(self.context.environmental_dataset)
         specfile = spec.get('occur.csv')
         envfile = env.get('current_asc.zip')
-        import ipdb; ipdb.set_trace()
 
         async = getUtility(IAsyncService)
-        #queue = async.getQueues()['']
-        job = async.queueJob(execute, self.context, envfile, specfile)
+        # TODO: default queue quota is 1. either set it to a defined value (see: plone.app.asnc.subscriber)
+        #       or create and oubmit job manually
+        #job = async.queueJob(execute, self.context, envfile, specfile)
+        jobinfo = (execute, self.context, (envfile, specfile), {})
+        job = async.wrapJob(jobinfo)
+        queue = async.getQueues()['']
+        job = queue.put(job)
+        # don't forget the plone.app.async notification callbacks
+        job.addCallbacks(success=service.job_success_callback,
+                         failure=service.job_failure_callback)
+
         IStatusMessage(self.request).add(u'Job submitted {}'.format(job.status), type='info')
         self.request.response.redirect(self.context.absolute_url())
