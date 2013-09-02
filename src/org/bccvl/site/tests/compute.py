f2d1314d96ca762ef735e6fad92bb6a21c5898ff
Gerhard Weis <gerhard.weis@gmail.com>
2013-09-02 14:34:39 +1000

    add dummy compute functions for testing

---
+++ src/org/bccvl/site/tests/compute.py
@@ -0,0 +1,23 @@
+from org.bccvl.compute import utils
+
+
+def testalgorithm(experiment):
+    """
+    An algorithm mack function, that generates some test result data.
+    """
+    # 1. create tempfolder for result files
+    # 2. create some result files
+    pass
+    #utils.store_results(experiment, tmpdir)
+    # 3. clean up tmpdir
+
+
+def failingtestalgorithm(experiment):
+    """
+    An algorithm that always fails.
+    """
+    # 1. create tempfolder for result files
+    # 2. create some error files
+    pass
+    #utils.store_results(experiment, tmpdir)
+    # 3. clean up tmpdir
