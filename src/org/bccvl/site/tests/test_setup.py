a817509600b972d2a269b1eefbb4f8e96c7980d5
Gerhard Weis <gerhard.weis@gmail.com>
2013-09-02 11:10:14 +1000

    added Add Experiment permission moved Genericsetup configuration to separate zcml file

--- src/org/bccvl/site/tests/test_setup.py
+++ src/org/bccvl/site/tests/test_setup.py
@@ -3,6 +3,12 @@ from org.bccvl.site import defaults
 from org.bccvl.site.testing import BCCVL_INTEGRATION_TESTING
 
 
+def selectedRoles(obj, permission):
+    for role in obj.rolesOfPermission(permission):
+        if role['selected'] == 'SELECTED':
+            yield role['name']
+
+
 class SiteSetupTest(unittest.TestCase):
 
     layer = BCCVL_INTEGRATION_TESTING
@@ -11,10 +17,22 @@ class SiteSetupTest(unittest.TestCase):
         portal = self.layer['portal']
 
         self.assertTrue(defaults.DATASETS_FOLDER_ID in portal)
-        dsf =  portal[defaults.DATASETS_FOLDER_ID]
+        dsf = portal[defaults.DATASETS_FOLDER_ID]
         self.assertTrue(defaults.DATASETS_SPECIES_FOLDER_ID in dsf)
         self.assertTrue(defaults.DATASETS_ENVIRONMENTAL_FOLDER_ID in dsf)
         self.assertTrue(defaults.DATASETS_CLIMATE_FOLDER_ID in dsf)
 
         self.assertTrue(defaults.FUNCTIONS_FOLDER_ID in portal)
         self.assertTrue(defaults.KNOWLEDGEBASE_FOLDER_ID in portal)
+
+    def test_add_experiment_permission(self):
+        portal = self.layer['portal']
+        pt = portal['portal_types']
+        fti = pt['org.bccvl.content.experiment']
+        self.assertEqual(fti.add_permission, 'org.bccvl.AddExperiment')
+
+    def test_roles(self):
+        portal = self.layer['portal']
+        roles = tuple(selectedRoles(portal, "org.bccvl: Add Experiment"))
+        self.assertEqual(roles,
+                         ('Manager', ))
