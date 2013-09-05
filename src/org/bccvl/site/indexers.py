f8ba381601b5981552240fde09059ce57e29e6c5
Gerhard Weis <gerhard.weis@gmail.com>
2013-09-06 02:14:20 +1000

    use KeywordIndex to store multiple values

--- src/org/bccvl/site/indexers.py
+++ src/org/bccvl/site/indexers.py
@@ -4,12 +4,14 @@ from gu.repository.content.interfaces import IRepositoryMetadata
 
 from .namespace import BCCPROP
 
+
 @indexer(IRepositoryItem)
 def repositoryItem_BCCDataGenre(object, *kw):
     graph = IRepositoryMetadata(object)
     return graph.value(graph.identifier, BCCPROP['datagenre'])
 
+
 @indexer(IRepositoryItem)
 def repositoryItem_BCCSpeciesLayer(object, *kw):
     graph = IRepositoryMetadata(object)
-    return graph.value(graph.identifier, BCCPROP['specieslayer'])
+    return tuple(graph.objects(graph.identifier, BCCPROP['specieslayer']))
