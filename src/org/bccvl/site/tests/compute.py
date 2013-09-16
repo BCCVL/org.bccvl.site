36f5dcc84049824a0d259df16276ea1a9b1132cf
Gerhard Weis <gerhard.weis@gmail.com>
2013-09-16 17:41:42 +1000

    create some dummy result files in test algorithm

--- src/org/bccvl/site/tests/compute.py
+++ src/org/bccvl/site/tests/compute.py
@@ -1,4 +1,7 @@
 from org.bccvl.compute import utils
+from tempfile import mkdtemp
+import os.path
+import shutil
 
 
 def testalgorithm(experiment):
@@ -6,10 +9,18 @@ def testalgorithm(experiment):
     An algorithm mack function, that generates some test result data.
     """
     # 1. create tempfolder for result files
-    # 2. create some result files
-    pass
-    #utils.store_results(experiment, tmpdir)
-    # 3. clean up tmpdir
+    tmpdir = mkdtemp()
+    try:
+        # 2. create some result files
+        for fname in ('testfile1.txt', ):
+            f = open(os.path.join(tmpdir, fname), 'w')
+            f.write(str(range(1, 10)))
+            f.close()
+        # 3. store results
+        utils.store_results(experiment, tmpdir)
+    finally:
+        # 4. clean up tmpdir
+        shutil.rmtree(tmpdir)
 
 
 def failingtestalgorithm(experiment):
