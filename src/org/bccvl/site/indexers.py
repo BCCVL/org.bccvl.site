d1966620d42e398f310152f5a255071acdd970ca
Gerhard Weis <gerhard.weis@gmail.com>
2013-09-24 07:02:33 +1000

    metadata is stored on datasets

--- src/org/bccvl/site/indexers.py
+++ src/org/bccvl/site/indexers.py
@@ -1,17 +1,17 @@
 from plone.indexer.decorator import indexer
-from gu.repository.content.interfaces import IRepositoryItem
+from org.bccvl.site.content.dataset import IDataset
 from gu.z3cform.rdf.interfaces import IGraph
 
 from .namespace import BCCPROP
 
 
-@indexer(IRepositoryItem)
-def repositoryItem_BCCDataGenre(object, *kw):
+@indexer(IDataset)
+def dataset_BCCDataGenre(object, *kw):
     graph = IGraph(object)
     return tuple(graph.objects(graph.identifier, BCCPROP['datagenre']))
 
 
-@indexer(IRepositoryItem)
-def repositoryItem_BCCSpeciesLayer(object, *kw):
+@indexer(IDataset)
+def dataset_BCCSpeciesLayer(object, *kw):
     graph = IGraph(object)
     return tuple(graph.objects(graph.identifier, BCCPROP['specieslayer']))
