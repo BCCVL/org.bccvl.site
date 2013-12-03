e97a635e189ad0a66c4e0b15b5dc8d4c217c5890
Gerhard Weis <g.weis@griffith.edu.au>
2013-12-03 17:23:07 +1000

    added index for environmental layers

--- src/org/bccvl/site/indexers.py
+++ src/org/bccvl/site/indexers.py
@@ -1,6 +1,7 @@
 from plone.indexer.decorator import indexer
 from org.bccvl.site.content.dataset import IDataset
 from gu.z3cform.rdf.interfaces import IGraph
+from org.bccvl.site.browser.xmlrpc import getbiolayermetadata
 
 from .namespace import BCCPROP
 
@@ -15,3 +16,11 @@ def dataset_BCCDataGenre(object, *kw):
 def dataset_BCCSpeciesLayer(object, *kw):
     graph = IGraph(object)
     return tuple(graph.objects(graph.identifier, BCCPROP['specieslayer']))
+
+
+@indexer(IDataset)
+def dataset_environmental_layer(object, **kw):
+    layers = getbiolayermetadata(object)
+    if layers:
+        return tuple(layers.keys())
+    return None
