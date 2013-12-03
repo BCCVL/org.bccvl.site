fe0321bc0a744a7d938f6c7a1f406c758725dbd8
Gerhard Weis <g.weis@griffith.edu.au>
2013-12-03 17:22:35 +1000

    get rid of grok

--- src/org/bccvl/site/vocabularies.py
+++ src/org/bccvl/site/vocabularies.py
@@ -1,47 +1,49 @@
 from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-from five import grok
-
-from org.bccvl.site import defaults
 from org.bccvl.site.api import QueryAPI
+from zope.interface import implementer
 
-# FIXME: get rid of grok :)
-# TODO: sort by title
 
-def _source_factory(name, apiFunc, token_key='UID'):
-    @grok.provider(IContextSourceBinder)
-    def _datasets_source(context):
+@implementer(IContextSourceBinder)
+class DataSetSourceBinder(object):
+
+    def __init__(self, name, apiFunc, tokenKey='UID'):
+        self.__name__ = name
+        self.apiFunc = apiFunc
+        self.tokenKey = tokenKey
+
+    def __call__(self, context):
         api = QueryAPI(context)
-        brains = getattr(api, apiFunc)()
-        terms = [
-            SimpleTerm(value=brain['UID'], token=brain[token_key], title=brain['Title'])
-            for brain in brains
-        ]
+        brains = getattr(api, self.apiFunc)()
+        terms = [SimpleTerm(value=brain['UID'],
+                            token=brain[self.tokenKey],
+                            title=brain['Title'])
+                 for brain in brains]
+        terms = sorted(terms, key=lambda o: o.title)
         return SimpleVocabulary(terms)
-    _datasets_source.__name__ = name # to help with debugging
-    return _datasets_source
+
 
 # species occurrence datasets
-species_presence_datasets_source = _source_factory(
+species_presence_datasets_source = DataSetSourceBinder(
     'species_presence_datasets_source', 'getSpeciesPresenceDatasets'
 )
 
-species_absence_datasets_source = _source_factory(
+species_absence_datasets_source = DataSetSourceBinder(
     'species_absence_datasets_source', 'getSpeciesAbsenceDatasets'
 )
 
-species_abundance_datasets_source = _source_factory(
+species_abundance_datasets_source = DataSetSourceBinder(
     'species_abundance_datasets_source', 'getSpeciesAbundanceDatasets'
 )
 
-environmental_datasets_source = _source_factory(
+environmental_datasets_source = DataSetSourceBinder(
     'environmental_datasets_source', 'getEnvironmentalDatasets'
 )
 
-future_climate_datasets_source = _source_factory(
+future_climate_datasets_source = DataSetSourceBinder(
     'future_climate_datasets_source', 'getFutureClimateDatasets'
 )
 
-functions_source = _source_factory(
+functions_source = DataSetSourceBinder(
     'functions_source', 'getFunctions', 'id'
 )
