daac275a936e1a21fd245f83097eef4066e14cac
Gerhard Weis <g.weis@griffith.edu.au>
2014-01-07 10:47:22 +1000

    create multiselect widget that uses checkboxes

---
+++ src/org/bccvl/site/browser/interfaces.py
@@ -0,0 +1,12 @@
+from z3c.form.interfaces import IOrderedSelectWidget, IWidget
+
+
+class IOrderedCheckboxWidget(IOrderedSelectWidget):
+    """
+    Interface for Ordered select widget that renders checkboxes
+    """
+
+
+class IDatasetsWidget(IWidget):
+
+    pass
--- src/org/bccvl/site/browser/widgets.py
+++ src/org/bccvl/site/browser/widgets.py
@@ -3,14 +3,8 @@ from zope.interface import implementer
 from zope.schema.interfaces import ISequence
 from z3c.form.interfaces import IFieldWidget,  IFormLayer
 from z3c.form.widget import FieldWidget
-from z3c.form.interfaces import IOrderedSelectWidget
 from z3c.form.browser.orderedselect import OrderedSelectWidget
-
-
-class IOrderedCheckboxWidget(IOrderedSelectWidget):
-    """
-    Interface for Ordered select widget that renders checkboxes
-    """
+from .interfaces import IDatasetsWidget, IOrderedCheckboxWidget
 
 
 @implementer(IOrderedCheckboxWidget)
