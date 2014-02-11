a99472cc5ea392ecbfbacea21a04605ed5e680aa
Gerhard Weis <g.weis@griffith.edu.au>
2014-02-11 13:55:04 +1000

    move form schemata into dataset content package

--- src/org/bccvl/site/browser/datasets_upload_view.py
+++ src/org/bccvl/site/browser/datasets_upload_view.py
@@ -2,20 +2,12 @@ from Acquisition import aq_inner
 from Products.Five import BrowserView
 from plone.dexterity.browser.add import DefaultAddForm
 from Products.CMFCore.utils import getToolByName
-from z3c.form.interfaces import IFormLayer
-from plone.z3cform import z2
-from z3c.form.form import AddForm
 
 from plone.app.dexterity.behaviors.metadata import IBasic
-from Products.CMFCore.interfaces import IDublinCore
 from z3c.form.field import Fields
-from org.bccvl.site.content.dataset import IDataset
-
-from gu.z3cform.rdf.schema import (RDFURIChoiceField,
-                                   RDFLiteralLineField,
-                                   RDFDateRangeField)
-from org.bccvl.site.namespace import BCCPROP, DWC
-from ordf.namespace import DC as DCTERMS
+from org.bccvl.site.content.dataset import (IDataset,
+                                            ISpeciesDataset,
+                                            ILayerDataset)
 
 
 # TODO: provenance field:
@@ -26,104 +18,7 @@ class SpeciesAddForm(DefaultAddForm):
     title = u"Species occurrence data"
     description = u"A set of occurrences for single species"
 
-    fields = (
-        Fields(IBasic) +
-        Fields(IDataset) +
-        Fields(
-            # restrict this to???
-            RDFURIChoiceField(
-                __name__='datagenre',
-                prop=BCCPROP['datagenre'],
-                required=False,
-                title=u'Data Genre',
-                vocabulary=u'http://namespaces.zope.org/z3c/form#DataGenreVocabulary'),
-            RDFURIChoiceField(
-                __name__='specieslayer',
-                prop=BCCPROP['specieslayer'],
-                required=False,
-                title=u'Species Layer',
-                vocabulary=u'http://namespaces.zope.org/z3c/form#SpeciesLayerVocabulary'),
-            RDFLiteralLineField(
-                __name__ = 'scientificName',
-                prop = DWC['scientificName'],
-                required = True,
-                title = u'Scientific name',
-                description = u'The full scientific name, with authorship and date information if known. When forming part of an Identification, this should be the name in lowest level taxonomic rank that can be determined. This term should not contain identification qualifications, which should instead be supplied in the IdentificationQualifier term.'),
-            RDFLiteralLineField(  # lsid
-                __name__='taxonID',
-                prop=DWC['taxonID'],
-                required=False,
-                title=u'Taxon ID',
-                description=u'''An identifier for the set of taxon information (data associated with the Taxon class). May be a global unique identifier or an identifier specific to the data set.
-
-                Examples: "8fa58e08-08de-4ac1-b69c-1235340b7001", "32567", "http://species.gbif.org/abies_alba_1753", "urn:lsid:gbif.org:usages:32567"'''
-                ),
-            RDFLiteralLineField(  # common Name,
-                __name__='vernacularName',
-                prop=DWC['vernacularName'],
-                required=False,
-                title=u'Common Name',
-                description=u'A common or vernacular name.',
-                ),
-
-
-
-
-
-            # fixed fields
-            # RDFURIChoiceField(
-            #     __name__='format',
-            #     prop=BCCPROP['format'],
-            #     required=False,
-            #     title=u'Format',
-            #     vocabulary=u'http://namespaces.zope.org/z3c/form#DataSetFormatVocabulary'),
-
-
-            # layer
-            # RDFURIChoiceField(
-            #     __name__='datatype',
-            #     prop=BCCPROP['datatype'],
-            #     required=False,
-            #     title=u'Type of Dataset',
-            #     vocabulary=u'http://namespaces.zope.org/z3c/form#DataSetTypeVocabulary'),
-
-            # RDFURIChoiceField(
-            #     __name__='resolution',
-            #     prop=BCCPROP['resolution'],
-            #     title=u'Resolution',
-            #     required=False,
-            #     vocabulary=u'http://namespaces.zope.org/z3c/form#ResolutionVocabulary'),
-            # RDFLiteralLineField(
-            #     __name__='resolutiono',
-            #     prop=BCCPROP['resolutionother'],
-            #     required=False,
-            #     title=u'Resolution (other)'),
-            # RDFDateRangeField(
-            #     __name__='temporal',
-            #     prop=DCTERMS['temporal'],
-            #     required=False,
-            #     title=u'Temporal coverage'),
-            # RDFURIChoiceField(
-            #     __name__='emissionscenario',
-            #     prop=BCCPROP['emissionscenario'],
-            #     required=False,
-            #     title=u'Emission Scenario',
-            #     vocabulary=u'http://namespaces.zope.org/z3c/form#EmissionScenarioVocabulary'),
-            # RDFURIChoiceField(
-            #     __name__='gcm',
-            #     prop=BCCPROP['gcm'],
-            #     required=False,
-            #     title=u'Global Climate Model',
-            #     vocabulary=u'http://namespaces.zope.org/z3c/form#GlobalClimateModelVocabulary'),
-
-            # # not needed?
-            # RDFLiteralLineField(
-            #     __name__='formato',
-            #     prop=DCTERMS['format'],
-            #     required=False,
-            #     title=u'Data format (other)'),
-        )
-    )
+    fields = Fields(IBasic, IDataset, ISpeciesDataset)
 
     def updateFields(self):
         pass
@@ -131,73 +26,7 @@ class SpeciesAddForm(DefaultAddForm):
 
 class RasterAddForm(DefaultAddForm):
 
-    # moref fields;
-    #-> projection, spatial units, spatial coverage,
-    #-> pixel units, (value space)
-
-    fields = Fields(
-        Fields(IBasic) +
-        Fields(IDataset) +
-        Fields(
-            # restrict this to???
-            RDFURIChoiceField(
-                __name__='datagenre',
-                prop=BCCPROP['datagenre'],
-                required=False,
-                title=u'Data Genre',
-                vocabulary=u'http://namespaces.zope.org/z3c/form#DataGenreVocabulary'),
-            # fixed fields
-            # RDFURIChoiceField(
-            #     __name__='format',
-            #     prop=BCCPROP['format'],
-            #     required=False,
-            #     title=u'Format',
-            #     vocabulary=u'http://namespaces.zope.org/z3c/form#DataSetFormatVocabulary'),
-
-            # layer
-            RDFURIChoiceField(
-                __name__='datatype',
-                prop=BCCPROP['datatype'],
-                required=False,
-                title=u'Type of Dataset',
-                vocabulary=u'http://namespaces.zope.org/z3c/form#DataSetTypeVocabulary'),
-            RDFURIChoiceField(
-                __name__='resolution',
-                prop=BCCPROP['resolution'],
-                title=u'Resolution',
-                required=False,
-                vocabulary=u'http://namespaces.zope.org/z3c/form#ResolutionVocabulary'),
-            RDFLiteralLineField(
-                __name__='resolutiono',
-                prop=BCCPROP['resolutionother'],
-                required=False,
-                title=u'Resolution (other)'),
-            RDFDateRangeField(
-                __name__='temporal',
-                prop=DCTERMS['temporal'],
-                required=False,
-                title=u'Temporal coverage'),
-            RDFURIChoiceField(
-                __name__='emissionscenario',
-                prop=BCCPROP['emissionscenario'],
-                required=False,
-                title=u'Emission Scenario',
-                vocabulary=u'http://namespaces.zope.org/z3c/form#EmissionScenarioVocabulary'),
-            RDFURIChoiceField(
-                __name__='gcm',
-                prop=BCCPROP['gcm'],
-                required=False,
-                title=u'Global Climate Model',
-                vocabulary=u'http://namespaces.zope.org/z3c/form#GlobalClimateModelVocabulary'),
-
-            # # not needed?
-            # RDFLiteralLineField(
-            #     __name__='formato',
-            #     prop=DCTERMS['format'],
-            #     required=False,
-            #     title=u'Data format (other)'),
-        )
-    )
+    fields = Fields(IBasic, IDataset, ILayerDataset)
 
     def updateFields(self):
         pass
--- src/org/bccvl/site/content/dataset.py
+++ src/org/bccvl/site/content/dataset.py
@@ -4,6 +4,11 @@ from plone.dexterity.content import Item
 from zope.interface import implementer
 from org.bccvl.site import MessageFactory as _
 from plone.app.contenttypes.interfaces import IFile
+from gu.z3cform.rdf.schema import (RDFURIChoiceField,
+                                   RDFLiteralLineField,
+                                   RDFDateRangeField)
+from org.bccvl.site.namespace import BCCPROP, DWC
+from ordf.namespace import DC as DCTERMS
 
 
 class IDataset(form.Schema):
@@ -15,8 +20,112 @@ class IDataset(form.Schema):
         description=_(u"Data content"),
         required=True)
 
+    datagenre = RDFURIChoiceField(
+        prop=BCCPROP['datagenre'],
+        title=_(u'Data Genre'),
+        vocabulary=u'http://namespaces.zope.org/z3c/form#DataGenreVocabulary')
+
+        # fixed fields
+        # RDFURIChoiceField(
+        #     __name__='format',
+        #     prop=BCCPROP['format'],
+        #     required=False,
+        #     title=u'Format',
+        #     vocabulary=u'http://namespaces.zope.org/z3c/form#DataSetFormatVocabulary')
+
+
+        # layer
+        # RDFURIChoiceField(
+        #     __name__='datatype',
+        #     prop=BCCPROP['datatype'],
+        #     required=False,
+        #     title=u'Type of Dataset',
+        #     vocabulary=u'http://namespaces.zope.org/z3c/form#DataSetTypeVocabulary'),
+        # # not needed?
+        # RDFLiteralLineField(
+        #     __name__='formato',
+        #     prop=DCTERMS['format'],
+        #     required=False,
+        #     title=u'Data format (other)'),
+
 
 @implementer(IDataset, IFile)
 class Dataset(Item):
 
     pass
+
+
+class ISpeciesDataset(form.Schema):
+    """
+    a schema to drive the forms for species data sets
+    """
+    specieslayer = RDFURIChoiceField(
+        prop=BCCPROP['specieslayer'],
+        required=False,
+        title=u'Species Layer',
+        vocabulary=u'http://namespaces.zope.org/z3c/form#SpeciesLayerVocabulary'
+    )
+
+    scientificName = RDFLiteralLineField(
+        prop = DWC['scientificName'],
+        required = True,
+        title = u'Scientific name',
+        description = u'The full scientific name, with authorship and date information if known. When forming part of an Identification, this should be the name in lowest level taxonomic rank that can be determined. This term should not contain identification qualifications, which should instead be supplied in the IdentificationQualifier term.'
+    )
+
+    taxonID = RDFLiteralLineField(  # lsid
+        prop=DWC['taxonID'],
+        required=False,
+        title=u'Taxon ID',
+        description=u'''An identifier for the set of taxon information (data associated with the Taxon class). May be a global unique identifier or an identifier specific to the data set.
+
+                Examples: "8fa58e08-08de-4ac1-b69c-1235340b7001", "32567", "http://species.gbif.org/abies_alba_1753", "urn:lsid:gbif.org:usages:32567"'''
+    )
+
+    vernacularName = RDFLiteralLineField(  # common Name,
+        prop=DWC['vernacularName'],
+        required=False,
+        title=u'Common Name',
+        description=u'A common or vernacular name.',
+    )
+
+
+class ILayerDataset(form.Schema):
+
+    # moref fields;
+    #-> projection, spatial units, spatial coverage,
+    #-> pixel units, (value space)
+
+    datatype = RDFURIChoiceField(
+        prop=BCCPROP['datatype'],
+        required=False,
+        title=u'Type of Dataset',
+        vocabulary=u'http://namespaces.zope.org/z3c/form#DataSetTypeVocabulary')
+
+    resolution = RDFURIChoiceField(
+        prop=BCCPROP['resolution'],
+        title=u'Resolution',
+        required=False,
+        vocabulary=u'http://namespaces.zope.org/z3c/form#ResolutionVocabulary')
+
+    resolutiono = RDFLiteralLineField(
+        prop=BCCPROP['resolutionother'],
+        required=False,
+        title=u'Resolution (other)')
+
+    temporal = RDFDateRangeField(
+        prop=DCTERMS['temporal'],
+        required=False,
+        title=u'Temporal coverage')
+
+    emissionscenario = RDFURIChoiceField(
+        prop=BCCPROP['emissionscenario'],
+        required=False,
+        title=u'Emission Scenario',
+        vocabulary=u'http://namespaces.zope.org/z3c/form#EmissionScenarioVocabulary')
+
+    gcm = RDFURIChoiceField(
+        prop=BCCPROP['gcm'],
+        required=False,
+        title=u'Global Climate Model',
+        vocabulary=u'http://namespaces.zope.org/z3c/form#GlobalClimateModelVocabulary')
