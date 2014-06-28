e477c19fc33c19154bb1259383060bc098a424c3
Gerhard Weis <g.weis@griffith.edu.au>
2014-06-28 14:19:05 +1000

    added dataset_tools, a help view to help with templating moved all "tmeplate" methods from dataset_listing view to this help view

--- src/org/bccvl/site/browser/interfaces.py
+++ src/org/bccvl/site/browser/interfaces.py
@@ -1,4 +1,31 @@
+from zope.interface import Interface
 from z3c.form.interfaces import IOrderedSelectWidget, IWidget
+
+
+class IDatasetTools(Interface):
+
+    def get_transition(itemob=None):
+        """
+        return list of possible workflow transitions on current context or
+        itemob?
+        """
+
+    def get_download_info(item=None):
+        """
+        get download info dict for current context or item.
+        """
+
+    def can_modify(itemob=None):
+        """
+        return true if current user has permission cmf.ModifyPortalContent
+        on context or itemob
+        """
+
+    def local_roles_action(itemobj=None):
+        """
+        return action info dictionary for local_roles action.
+        ("Sharing Options")
+        """
 
 
 class IOrderedCheckboxWidget(IOrderedSelectWidget):
