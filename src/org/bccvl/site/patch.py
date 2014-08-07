f5602afee7ec2005b4d2413f25d635c043cbc980
Gerhard Weis <g.weis@griffith.edu.au>
2014-08-07 12:51:56 +1000

    hotfix: don't let plone move files into the storage. (doesn't work with transaction abort/retry)

--- src/org/bccvl/site/patch.py
+++ src/org/bccvl/site/patch.py
@@ -10,3 +10,34 @@
 from plone.app.dexterity.behaviors.exclfromnav import IExcludeFromNavigation
 
 IExcludeFromNavigation['exclude_from_nav'].required = False
+
+
+# plone.namedfile uses blob.consumeFile when loading data from a file
+# object/descriptor consumeFile tries to be efficient and moves the
+# file instead of copying it. This may have unwanted side effects,
+# like on a transaction abort it won't restore the original file. (bad
+# for conflicterror retries)
+from zope.interface import implements
+from plone.namedfile.interfaces import IStorage
+from plone.namedfile.interfaces import NotStorable
+import shutil
+
+
+class FileDescriptorStorable(object):
+    implements(IStorage)
+
+    def store(self, data, blob):
+        if not isinstance(data, file):
+            raise NotStorable("Could not store data (not of 'file').")
+
+        # The original used consumeFile which tries to move a file if possible
+        # filename = getattr(data, "name", None)
+        # if filename is not None:
+        #     blob.consumeFile(filename)
+        #     return
+        # Let us use any file object and just copy it
+        import logging
+        logging.getLogger("BLOB PATCH").info("USE MY BLOB STORAGE ADAPTER")
+        dest = blob.open('w')
+        # TODO: should we seek to 0?
+        shutil.copyfileobj(data, dest)
