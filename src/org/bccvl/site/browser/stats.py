9f2d3e82c5e240774b9aa1fabbcb7ccb9a424722
Matthew Trevor <matthew.trevor@griffith.edu.au>
2015-05-06 15:53:07 +1000

    Distinguished between successful & failed datasets Added count of user added datasets

--- src/org/bccvl/site/browser/stats.py
+++ src/org/bccvl/site/browser/stats.py
@@ -3,7 +3,11 @@ from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from plone import api
 from plone.app.contenttypes.interfaces import IFolder
-from ..content.interfaces import IExperiment, IDataset
+from ..content.interfaces import (
+    IExperiment, 
+    IDataset,
+    IRemoteDataset,
+)
 
 def _count_classes(result_seq):
     objects = (x._unrestrictedGetObject() for x in result_seq)
@@ -34,10 +38,22 @@ class StatisticsView(BrowserView):
         )
 
         datasets_path = dict(query='/'.join(self.context.datasets.getPhysicalPath()))
-        self._datasets_in = _search(
+        self._datasets_added = _search(
             path=datasets_path,
             object_provides=IDataset.__identifier__,
+            job_state=('COMPLETED', 'QUEUED', 'RUNNING'),
         )
+        self._datasets_added_remote = _search(
+            path=datasets_path,
+            object_provides=IRemoteDataset.__identifier__,
+            job_state=('COMPLETED', 'QUEUED', 'RUNNING'),
+        )
+        self._datasets_added_failed = _search(
+            path=datasets_path,
+            object_provides=IDataset.__identifier__,
+            job_state='FAILED',
+        ) 
+
         self._datasets_gen = _search(
             path=experiments_path,
             object_provides=IDataset.__identifier__,
@@ -57,13 +73,29 @@ class StatisticsView(BrowserView):
         return len(self._institutions)
     
     def datasets(self):
-        return self.datasets_added + self.datasets_generated
+        return self.datasets_added() + self.datasets_generated()
 
     def datasets_added(self):
-        return len(self._datasets_in)
+        return len(self._datasets_added)
+
+    def datasets_added_remote(self):
+        return len(self._datasets_added_remote)
+
+    def datasets_added_local(self):
+        return self.datasets_added() - self.datasets_added_remote()
+
+    def datasets_added_users(self):
+        datasets = (x._unrestrictedGetObject() for x in self._datasets_added)
+        owners = (x.getOwner() for x in datasets)
+        return len(
+            [ x for x in owners if x.getUserName not in ('admin', 'bccvl',) ]
+        )
+
+    def datasets_added_failed(self):
+        return len(self._datasets_added_failed)
     
     def datasets_added_types(self):
-        return _count_classes(self._datasets_in).iteritems()
+        return _count_classes(self._datasets_added).iteritems()
 
     def datasets_generated(self):
         return len(self._datasets_gen)
