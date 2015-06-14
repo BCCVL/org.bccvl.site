3576156428ceb925b934d828cd051058da865cd0
Gerhard Weis <g.weis@griffith.edu.au>
2015-06-15 08:15:50 +1000

    prepare support for wsgi deployment

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
         'collective.z3cform.widgets',
         #'collective.deletepermission', careful it interfers with delete buttons when not activated
         #'collective.z3cform.chosen',
-        'plone.app.folderui',
         'borg.localrole',
         'plone.app.contenttypes',
         'decorator',
@@ -81,13 +80,25 @@ setup(
         #'plone.app.relationfield',
     ],
     extras_require={
-        'test': ['plone.app.testing',
-                 'unittest2'],
-        'deprecated':  ['gu.repository.content',
-                        'gu.plone.rdf',
-                        'dexterity.membrane',
-                        ],
-        'experimental': ['eea.facetednavigation']
+        'test': [
+            'plone.app.testing',
+            'unittest2'
+        ],
+        'deprecated':  [
+            'gu.repository.content',
+            'gu.plone.rdf',
+            'dexterity.membrane',  # pulls in collective.indexing (persistent tool)
+            'plone.app.folderui',
+        ],
+        'experimental': [
+            'eea.facetednavigation'
+        ],
+        'wsgi': [
+            'Paste',
+            'PasteScript',
+            'repoze.tm2',
+            'repoze.retry'
+        ]
     },
 
     entry_points="""
