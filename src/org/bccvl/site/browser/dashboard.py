8f7412bae71de13198b2a0658d0676d8dd713ab8
Gerhard Weis <g.weis@griffith.edu.au>
2015-07-07 08:50:46 +1000

    move get_state_css function into experiment_tools helper view

--- src/org/bccvl/site/browser/dashboard.py
+++ src/org/bccvl/site/browser/dashboard.py
@@ -1,6 +1,5 @@
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
-from org.bccvl.site.interfaces import IJobTracker
 from org.bccvl.site.content.interfaces import IDataset,  IExperiment
 from org.bccvl.site import defaults
 
@@ -15,7 +14,7 @@ class DashboardView(BrowserView):
     def num_datasets(self):
         return len(self.pc.searchResults(
             path='/'.join(self.portal.datasets.getPhysicalPath()),
-            job_state=("COMPLETED", "QUEUED", "RUNNING"),            
+            job_state=("COMPLETED", "QUEUED", "RUNNING"),
             object_provides=IDataset.__identifier__))
 
     def num_experiments(self):
@@ -27,7 +26,7 @@ class DashboardView(BrowserView):
         return self.pc.searchResults(
             path='/'.join(self.portal[defaults.DATASETS_FOLDER_ID].getPhysicalPath()),
             object_provides=IDataset.__identifier__,
-            job_state=("COMPLETED", "QUEUED", "RUNNING"),            
+            job_state=("COMPLETED", "QUEUED", "RUNNING"),
             sort_on='modified',
             sort_order='descending',
         )[:3]
@@ -40,14 +39,3 @@ class DashboardView(BrowserView):
             sort_order='descending',
             sort_limit=3
         )[:3]
-
-    def get_state_css(self, brain):
-        css_map = {
-            None: 'success',
-            'COMPLETED': 'success',
-            'FAILED': 'error',
-            'REMOVED': 'removed'
-        }
-        # check job_state and return either success, error or block
-        job_state = IJobTracker(brain.getObject()).state
-        return css_map.get(job_state, 'info')
