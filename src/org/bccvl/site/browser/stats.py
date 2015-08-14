a0185981892b7e93649527c4c2483cc7848427f1
Gerhard Weis <g.weis@griffith.edu.au>
2015-08-14 12:31:27 +1000

    add number of experiments per user to user list

--- src/org/bccvl/site/browser/stats.py
+++ src/org/bccvl/site/browser/stats.py
@@ -1,33 +1,27 @@
-import itertools
 from collections import Counter
 from operator import itemgetter
 from datetime import datetime, timedelta
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from plone import api
+from plone.app.uuid.utils import uuidToCatalogBrain
 from plone.app.contenttypes.interfaces import IFolder
 from ..content.interfaces import (
-    IExperiment, 
+    IExperiment,
     IDataset,
     IRemoteDataset,
 )
 
-def _count_classes(result_seq):
-    objects = (x._unrestrictedGetObject() for x in result_seq)
-    return Counter(
-        o.__class__.__name__ for o in objects
-    )
-
 class StatisticsView(BrowserView):
     def __call__(self):
         _search = getToolByName(self.context, 'portal_catalog').unrestrictedSearchResults
-        
+
         experiments_path = dict(query='/'.join(self.context.experiments.getPhysicalPath()))
-        _experiments = _search(
+        self._experiments = _search(
             path=experiments_path,
             object_provides=IExperiment.__identifier__
         )
-        self._experiments = [ x._unrestrictedGetObject() for x in _experiments ]
+        # self._experiments = [ x._unrestrictedGetObject() for x in _experiments ]
 
         self._experiments_pub = _search(
             path=experiments_path,
@@ -55,7 +49,7 @@ class StatisticsView(BrowserView):
             path=datasets_path,
             object_provides=IDataset.__identifier__,
             job_state='FAILED',
-        ) 
+        )
 
         self._datasets_gen = _search(
             path=experiments_path,
@@ -64,16 +58,20 @@ class StatisticsView(BrowserView):
 
         self._users = api.user.get_users()
         emails = (u.getProperty('email') for u in self._users)
-        self._institutions = Counter(e.split('@')[-1] for e in emails) 
+        self._institutions = Counter(e.split('@')[-1] for e in emails)
 
         return super(StatisticsView, self).__call__()
 
     def users_total(self):
         return len(self._users)
-    
+
     def users_new(self):
         return 999999
-    
+
+    def users_experiments(self):
+        # returns a dict with user id's as key and number of experiments run
+        return Counter(x.Creator for x in self._experiments)
+
     def users_active(self):
         furthest_login_time = datetime.now() - timedelta(days=90)
         last_login_times = (x.getProperty('last_login_time').utcdatetime() for x in self._users)
@@ -87,7 +85,7 @@ class StatisticsView(BrowserView):
 
     def institutions_total(self):
         return len(self._institutions)
-    
+
     def datasets(self):
         return self.datasets_added() + self.datasets_generated()
 
@@ -109,34 +107,39 @@ class StatisticsView(BrowserView):
 
     def datasets_added_failed(self):
         return len(self._datasets_added_failed)
-    
-    def datasets_added_types(self):
-        return _count_classes(self._datasets_added).iteritems()
 
     def datasets_generated(self):
         return len(self._datasets_gen)
-    
+
     def experiments_run(self):
         return len(self._experiments)
-    
+
     def experiments_published(self):
         return len(self._experiments_pub)
-    
+
     def experiment_types(self):
-        experiments = Counter(
-            x.__class__.__name__.replace('Experiment','') for x in self._experiments
-        )
-        by_count = itemgetter(1) # sort by value
-        return sorted(experiments.iteritems(), key=by_count, reverse=True)
-        
+        experiments = Counter(x.Type for x in self._experiments)
+        return experiments.most_common()
+
     def algorithm_types(self):
-        algorithms = (x.algorithms for x in self._experiments if hasattr(x, 'algorithms'))
-        functions = (x.functions for x in self._experiments if hasattr(x, 'functions'))
-        flat_functions = (i for y in functions for i in y)
-        algorithm_objs = (api.content.get(UID=x) for x in itertools.chain(algorithms, flat_functions))
-        algorithm_types= Counter(x.title for x in algorithm_objs)
-        by_count = itemgetter(1) # sort by value
-        return sorted(algorithm_types.iteritems(), key=by_count, reverse=True)
-        
+        func_obj_cache = {}
+
+        def func_ids():
+            for x in self._experiments:
+                exp = x._unrestrictedGetObject()
+                if hasattr(exp, 'functions'):
+                    for funcid in exp.functions:
+                        yield funcid
+                if hasattr(exp, 'algorithm'):
+                    yield exp.algorithm
+
+        def get_func_obj(funcid):
+            if funcid not in func_obj_cache:
+                func_obj_cache[funcid] = uuidToCatalogBrain(funcid)
+            return func_obj_cache[funcid]
+
+        algorithm_types = Counter(get_func_obj(x).Title for x in func_ids())
+        return algorithm_types.most_common()
+
     def jobs(self):
         return len(self._jobs)
