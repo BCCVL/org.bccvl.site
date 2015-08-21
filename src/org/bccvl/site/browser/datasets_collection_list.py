abc859df7ea7e7cf02b1085ae47a42f325791ef3
Gerhard Weis <g.weis@griffith.edu.au>
2015-08-21 15:14:49 +1000

    also set default sorting when browsing by collection

--- src/org/bccvl/site/browser/datasets_collection_list.py
+++ src/org/bccvl/site/browser/datasets_collection_list.py
@@ -1,3 +1,5 @@
+from urllib import urlencode
+
 from Products.Five import BrowserView
 
 from eea.facetednavigation.interfaces import ICriteria
@@ -10,19 +12,30 @@ class DatasetsCollectionListView(BrowserView):
         super(DatasetsCollectionListView, self).__init__(context, request)
         self.datasets_url = self.context.absolute_url()
         self.criteria = ICriteria(self.context, {})
+        self.defaults = {}
+        self.criterion = None
         for criterion in self.criteria.values():
             if criterion.widget == 'pathselect':
                 self.criterion = criterion
-                return
-        self.criterion = None
+            if criterion.widget == 'sorting':
+                default = criterion.default
+                if not default:
+                    continue
+                if '(reverse)' in default:
+                    default = default.replace('(reverse)', '', 1)
+                    self.defaults['reversed'] = True
+                self.defaults[criterion.getId()] = default
 
     def get_browse_link(self, uuid):
         # return link into datasets facetedview to filter given collection
         collection = uuidToCatalogBrain(uuid)
         if not collection:
             return self.datasets_url
+        params = dict(self.defaults)
         if self.criterion:
-            return "{}#{}={}".format(self.datasets_url, self.criterion.getId(), collection.UID)
+            params[self.criterion.getId()] = collection.UID
+            return "{}#{}".format(self.datasets_url, urlencode(params))
         # fallback to original datasets_listing_view
         groupid = collection.getObject().__parent__.getId()
-        return "{}/?datasets.filter.source%3Alist={}-{}".format(self.datests_url, groupid, collection.getId)
+        params['datasets.filter.source:list'] = '{}-{}'.format(self.datesets_url, groupid, collection.getId)
+        return "{}/?{}" . format(self.datasets_url, urlencode(params))
