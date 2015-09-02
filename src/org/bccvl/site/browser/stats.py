1911d240671ad73319bc60398758e9d94047306c
Yong Liaw <y.liaw@griffith.edu.au>
2015-09-02 14:30:32 +1000

    BCCVLB-133: Add average runtime for experiment and algorithm runs.

--- src/org/bccvl/site/browser/stats.py
+++ src/org/bccvl/site/browser/stats.py
@@ -14,6 +14,7 @@ from ..content.interfaces import (
 
 class StatisticsView(BrowserView):
     def __call__(self):
+        self.func_obj_cache = {}
         _search = getToolByName(self.context, 'portal_catalog').unrestrictedSearchResults
 
         experiments_path = dict(query='/'.join(self.context.experiments.getPhysicalPath()))
@@ -120,10 +121,60 @@ class StatisticsView(BrowserView):
     def experiment_types(self):
         experiments = Counter(x.Type for x in self._experiments)
         return experiments.most_common()
+        
+    def experiment_average_runtimes(self):
+        runtime = {}
+        count = {}
+        for x in self._experiments:
+            exp = x._unrestrictedGetObject()
+            if not hasattr(exp, 'runtime'):
+                continue
+            if runtime.has_key(x.Type):
+                runtime[x.Type] += exp.runtime
+                count[x.Type] += 1
+            else:
+                runtime[x.Type] = exp.runtime
+                count[x.Type] = 1
+        for tp in runtime.keys():
+            runtime[tp] /= count[tp]
+            runtime[tp] = "%.1f seconds" %(runtime[tp])
+        return runtime                 
+
+    def algorithm_average_runtimes(self):
+        runtime_sum = {}
+        count = {}
+        runtime_mean = {}
+        for x in self._experiments:                
+            #if x.Type not in ['org.bccvl.content.sdmexperiment', 'org.bccvl.content.speciestraitsexperiment']:
+            if x.Type not in ['SDM Experiment', 'Species Traits Modelling']:
+                continue
+            # Get the runtime each algorithm for each experiement type from the result file
+            # An algorithm is mutually exclusive for SDM or Species Traits experiement.
+            exp = x._unrestrictedGetObject()
+            for r in exp.values():                
+                import json
+                algid = r.job_params.get('function') or r.job_params.get('algorithm')
+                # Initialise statistic variables for each algorithm
+                if algid:
+                    runtime_sum[algid] = 0.0
+                    count[algid] = 0
+                    runtime_mean[algid] = 0.0
+                else:
+                    continue                    
+                if not r.has_key('pstats.json'):
+                    continue
+                js = json.loads(r['pstats.json'].file.data)
+                runtime = js['rusage'].get('ru_utime', -1.0) + js['rusage'].get('ru_stime', -1.0)
+                if runtime >= 0.0:
+                    runtime_sum[algid] += runtime
+                    count[algid] += 1
+                    runtime_mean[algid] = runtime_sum[algid]/count[algid]
+                    
+        for i in runtime_mean.keys():
+            runtime_mean[i] = "%.1f seconds" %(runtime_mean[i])
+        return runtime_mean
 
     def algorithm_types(self):
-        func_obj_cache = {}
-
         def func_ids():
             for x in self._experiments:
                 exp = x._unrestrictedGetObject()
@@ -132,14 +183,16 @@ class StatisticsView(BrowserView):
                         yield funcid
                 if hasattr(exp, 'algorithm'):
                     yield exp.algorithm
-
-        def get_func_obj(funcid):
-            if funcid not in func_obj_cache:
-                func_obj_cache[funcid] = uuidToCatalogBrain(funcid)
-            return func_obj_cache[funcid]
-
-        algorithm_types = Counter(get_func_obj(x).Title for x in func_ids())
+        
+        algorithm_types = Counter(self.get_func_obj(x).getId for x in func_ids())
         return algorithm_types.most_common()
+
+    def get_func_obj(self, uuid=None, funcid=None):
+        if (funcid is None or funcid in self.func_obj_cache) and uuid is not None:
+            brain = uuidToCatalogBrain(uuid)
+            self.func_obj_cache[brain.getId] = brain
+            return self.func_obj_cache[brain.getId]
+        return self.func_obj_cache[funcid]
 
     def jobs(self):
         return len(self._jobs)
