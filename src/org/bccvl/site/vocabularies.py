193c1dd07823cdc1e48a567c5641c162d5824032
Gerhard Weis <g.weis@griffith.edu.au>
2015-10-27 07:03:22 +1000

    make gamlss visible on species traits algorithms

--- src/org/bccvl/site/vocabularies.py
+++ src/org/bccvl/site/vocabularies.py
@@ -146,7 +146,7 @@ traits_functions_source = CatalogVocabularyFactory(
         # },
         'object_provides': 'org.bccvl.site.content.function.IFunction',
         # FIXME: find another way to separate SDM and traits "functions"
-        'id': ['lm', 'speciestrait_glm', 'speciestrait_gam',
+        'id': ['lm', 'speciestrait_glm', 'speciestrait_gam', 'gamlss',
                'aov', 'manova'],
         'sort_on': 'sortable_title',
     },
