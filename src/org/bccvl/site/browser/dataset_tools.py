3634dfba7aae31d701baa95dacdb5ddba36efa20
Gerhard Weis <g.weis@griffith.edu.au>
2016-03-21 12:57:20 +1000

    show multi species dataset input on overlay/compare pages

--- src/org/bccvl/site/browser/dataset_tools.py
+++ src/org/bccvl/site/browser/dataset_tools.py
@@ -4,7 +4,7 @@ from zope.interface import implementer
 from plone.app.uuid.utils import uuidToObject, uuidToCatalogBrain
 from plone.app.contentlisting.interfaces import IContentListingObject
 from plone import api
-from org.bccvl.site.content.interfaces import ISDMExperiment, IProjectionExperiment
+from org.bccvl.site.content.interfaces import ISDMExperiment, IMSDMExperiment, IProjectionExperiment
 from org.bccvl.site.interfaces import IDownloadInfo, IBCCVLMetadata
 from org.bccvl.site.job.interfaces import IJobTracker
 from org.bccvl.site.browser.interfaces import IDatasetTools
@@ -303,6 +303,12 @@ class DatasetTools(BrowserView):
             # TODO: once available include pesudo absences from result
             for dsuuid in (context.species_occurrence_dataset,
                            context.species_absence_dataset):
+                brain = uuidToCatalogBrain(dsuuid)
+                if brain:
+                    yield brain
+        elif IMSDMExperiment.providedBy(context):
+            # muilt species sdm inputs
+            for dsuuid in (context.species_occurrence_collections):
                 brain = uuidToCatalogBrain(dsuuid)
                 if brain:
                     yield brain
