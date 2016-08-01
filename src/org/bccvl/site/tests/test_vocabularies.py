ec1198148e59f635d50eaba5000f2e4076a028e3
Gerhard Weis <g.weis@griffith.edu.au>
2016-08-01 13:46:09 +1000

    fix vocabulary tests

--- src/org/bccvl/site/tests/test_vocabularies.py
+++ src/org/bccvl/site/tests/test_vocabularies.py
@@ -47,7 +47,7 @@ class GCMSourceTest(unittest.TestCase):
         self.assertIn(u'cccma-cgcm31', source)
         term = source.getTerm(u'cccma-cgcm31')
         self.assertEqual(term.value, u'cccma-cgcm31')
-        self.assertEqual(len(source), 37)
+        self.assertEqual(len(source), 38)
 
 
 class EMSCSourceTest(unittest.TestCase):
