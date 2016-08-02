618515a38d568592cb7177e64939dc91e8776afe
Yong Liaw <y.liaw@griffith.edu.au>
2016-08-02 17:31:34 +1000

    BCCVLB-513: Add support for aekos import.

--- src/org/bccvl/site/utils.py
+++ src/org/bccvl/site/utils.py
@@ -129,5 +129,7 @@ def build_ala_import_task(lsid, dataset, request):
     results_dir = get_results_dir(dataset, request)
     if dataset.dataSource == 'gbif':
         return datamover.pull_occurrences_from_gbif.si(lsid, results_dir, context)
+    elif dataset.dataSource == 'aekos':
+        return datamover.pull_occurrences_from_aekos.si(lsid, results_dir, context)
     else:
         return datamover.pull_occurrences_from_ala.si(lsid, results_dir, context)
