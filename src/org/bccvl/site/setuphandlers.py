524db20671f854e481a8eba0d6489fa4556481ed
Gerhard Weis <g.weis@griffith.edu.au>
2016-08-18 11:59:23 +1000

    add aekos collection add upgradestep for aekos collection

--- src/org/bccvl/site/setuphandlers.py
+++ src/org/bccvl/site/setuphandlers.py
@@ -665,3 +665,19 @@ def upgrade_240_250_1(context, logger=None):
     setup.runImportStepFromProfile(PROFILE_ID, 'org.bccvl.site.facet')
     # update theme (reimport it?)
     setup.runImportStepFromProfile(THEME_PROFILE_ID, 'plone.app.theming')
+
+
+def upgrade_250_260_1(context, logger=None):
+    if logger is None:
+        logger = LOG
+    # Run GS steps
+    portal = api.portal.get()
+    setup = api.portal.get_tool('portal_setup')
+    # update permissions on actions
+    setup.runImportStepFromProfile(PROFILE_ID, 'actions')
+    # update vocabularies
+    setup.runImportStepFromProfile(PROFILE_ID, 'plone.app.registry')
+    # update initial site content and r scripts
+    setup.runImportStepFromProfile(PROFILE_ID, 'org.bccvl.site.content')
+    # update facet settings
+    setup.runImportStepFromProfile(PROFILE_ID, 'org.bccvl.site.facet')
