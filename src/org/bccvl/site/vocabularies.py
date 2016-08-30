8a4bd5c1d760d6064562f33326cdf2e335c75950
Yong Liaw <y.liaw@griffith.edu.au>
2016-08-30 14:58:33 +1000

    BCCVLB-524: Add a new genre for Biodiverse analysis output.

--- src/org/bccvl/site/vocabularies.py
+++ src/org/bccvl/site/vocabularies.py
@@ -344,7 +344,8 @@ genre_vocabulary = SimpleVocabulary([
     SimpleTerm("DataGenreRAREW_CWE", "DataGenreRAREW_CWE", "Rarity whole - Corrected weighted rarity"),
     SimpleTerm("DataGenreRAREW_RICHNESS", "DataGenreRAREW_RICHNESS", "Rarity whole - Richness used in RAREW_CWE"),
     SimpleTerm("DataGenreRAREW_WE", "DataGenreRAREW_WE", "Rarity whole - weighted rarity"),
-    SimpleTerm("DataGenreBiodiverseModel", "DataGenreBiodiverseModel", "Biodiverse output"),
+    SimpleTerm("DataGenreBiodiverseModel", "DataGenreBiodiverseOutput", "Biodiverse output"),
+    SimpleTerm("DataGenreBiodiverseOutput", "DataGenreBiodiverseModel", "Biodiverse analysis output"),
     SimpleTerm("DataGenreEnsembleResult", "DataGenreEnsembleResult", "Ensembling output"),
     SimpleTerm("JobScript", "JobScript", "Job script"),
 ])
