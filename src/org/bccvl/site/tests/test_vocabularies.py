ad1b73e2831e936f1d474510715ae35f2312102c
Gerhard Weis <g.weis@griffith.edu.au>
2016-10-12 08:43:47 +1000

    update tests

--- src/org/bccvl/site/tests/test_vocabularies.py
+++ src/org/bccvl/site/tests/test_vocabularies.py
@@ -25,7 +25,7 @@ class LayerSourceTest(unittest.TestCase):
         self.assertIn(u'B01', source)
         term = source.getTerm(u'B01')
         self.assertEqual(term.value, u'B01')
-        self.assertEqual(len(source), 301)
+        self.assertEqual(len(source), 303)
 
 
 class GCMSourceTest(unittest.TestCase):
