d6dc82b7ccdef195f9dba6587660cb19bd7576c7
Gerhard Weis <g.weis@griffith.edu.au>
2016-11-03 17:59:29 +1100

    allow empty functions category use fake term no support empty functions category

--- src/org/bccvl/site/content/function.py
+++ src/org/bccvl/site/content/function.py
@@ -10,18 +10,18 @@ class IFunction(model.Schema):
 
     experiment_type = Choice(
         title=_(u"Experiment Type"),
-        description =_(u"The experiment type this toolkit can be used for."),
+        description=_(u"The experiment type this toolkit can be used for."),
         required=False,
         default=None,
         vocabulary='experiment_type_source'
-        )
+    )
 
     interpreter = Choice(
         title=_(u"Programming language"),
         description=_(u"The language the code is written in."),
         vocabulary="org.bccvl.site.programming_language_vocab",
         required=True,
-        )
+    )
 
     directives.widget('script',
                       rows=40)
@@ -29,15 +29,16 @@ class IFunction(model.Schema):
         title=_(u"Script"),
         description=_(u"The code being executed"),
         required=True,
-        )
+    )
 
     directives.widget('schema',
                       rows=40)
     schema = Text(
         title=_(u"Schema"),
-        description=_(u"A dexterity schema describing the input parameters for the algorithm"),
+        description=_(
+            u"A dexterity schema describing the input parameters for the algorithm"),
         required=True
-        )
+    )
 
     directives.widget('output',
                       rows=40)
@@ -45,14 +46,14 @@ class IFunction(model.Schema):
         title=_(u"Output mapping"),
         description=_(u"defines how to import experiment outputs"),
         required=True
-        )
+    )
 
     algorithm_category = Choice(
         title=_(u"Algorithm Category"),
         description=_(u"The category an algorithm belongs to"),
         vocabulary="org.bccvl.site.algorithm_category_vocab",
-        required=True,
-        )
+        required=False,
+    )
 
 # TODO: add validators:
 #    e.g. restrict the set of available methods; maybe setuptools
