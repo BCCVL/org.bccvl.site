44c54a4415b3c3ff3b58cfefb8264069f855dfe3
Yong Liaw <y.liaw@griffith.edu.au>
2017-10-10 10:12:41 +1000

    BCCVL-80: Download all the experiment results.

--- src/org/bccvl/site/browser/result_download.py
+++ src/org/bccvl/site/browser/result_download.py
@@ -29,87 +29,27 @@ class tmpfile_stream_iterator(filestream_iterator):
 class ResultDownloadView(BrowserView):
 
     def __call__(self):
-
-        # FIXME: This is a rather lengthy process, and should probably be turned into a background task... (maybe as part of a datamanager service?)
-
-        # 1. find all IBlobDataset/ IRemotedDataset/ IDataset objects within context
-        pc = getToolByName(self.context, 'portal_catalog')
-        brains = pc.searchResults(path='/'.join(self.context.getPhysicalPath()),
-                                  object_provides=[IBlobDataset.__identifier__,
-                                                   IRemoteDataset.__identifier__])
-        fname = None
         try:
             # create tmp file
             fd, fname = tempfile.mkstemp()
             fo = os.fdopen(fd, 'w')
             zfile = zipfile.ZipFile(fo, 'w')
 
-            metadata = {}
-
-            # the file/folder name for the zip
             zfilename = self.context.title
-            # iterate over files and add to zip
-            for brain in brains:
-                content = brain.getObject()
-                if IBlobDataset.providedBy(content):
-                    # If data is stored locally:
-                    arcname = '/'.join((zfilename, 'data', content.file.filename))
-                    # ob.file should be a NamedFile ... need to get fs name for that
-                    blobfile = content.file.openDetached()
-
-                    zfile.write(blobfile.name, arcname)
-                    blobfile.close()
-
-                elif IRemoteDataset.providedBy(content):
-                    # TODO: duplicate code from
-                    remoteUrl = getattr(content, 'remoteUrl', None)
-                    if remoteUrl is None:
-                        raise NotFound(self, 'remoteUrl', self.request)
-                    # get arcname from remoteUrl
-                    arcname = '/'.join((zfilename, 'data', os.path.basename(remoteUrl)))
-                    # FIXME: should check dataset downloaiable flag here,
-                    #       but assumption is, that this function can only be called on an experiment result folder....
-                    # TODO: duplicate code in browser/dataset.py:RemoteDatasetDownload.__call__
-                    # TODO: may not work in general... it always uses swift as remote url
-                    tool = getUtility(ISwiftUtility)
-                    try:
-                        url = tool.generate_temp_url(url=remoteUrl)
-                    except:
-                        url = remoteUrl
-                    # url is now the location from which we can fetch the file
-                    temp_file, _ = urlretrieve(url)
-                    zfile.write(temp_file, arcname)
-                    os.remove(temp_file)
-                else:
-                    # unknown type of Dataset
-                    # just skip it
-                    # TODO: Log warning or debug?
-                    continue
-                metadata[arcname] = getdsmetadata(content)
-            # all files are in ....
-            # TODO: add experiment result metadata
-
-            # put metadata into zip
-            # provenance data stored on result container
-            provdata = IProvenanceData(self.context)
-            if not provdata.data is None:
-                zfile.writestr('/'.join((zfilename, 'prov.ttl')),
-                               provdata.data)
-
-            # add exp metadata
-            expmetadata = IExperimentMetadata(self.context)
-            if not expmetadata.data is None:
-                zfile.writestr('/'.join((zfilename, 'expmetadata.txt')),
-                               expmetadata.data)
-
-            # add mets.xml
-            metsview = getMultiAdapter((self.context, self.request), name="mets.xml")
-            zfile.writestr('/'.join((zfilename, 'mets.xml')),
-                           metsview.render())
+            if self.context.portal_type in ('org.bccvl.content.sdmexperiment', 
+                                            'org.bccvl.content.msdmexperiment',
+                                            'org.bccvl.content.mmexperiment',
+                                            'org.bccvl.content.projectionexperiment',
+                                            'org.bccvl.content.biodiverseexperiment',
+                                            'org.bccvl.content.speciestraitsexperiment',
+                                            'org.bccvl.content.ensemble'):
+                for eid, eitem in self.context.contentItems():
+                    self._download_results(eitem, zfile)
+            else:
+                self._download_results(self.context, zfile)
 
             # finish zip file
             zfile.close()
-
             fo.close()
 
             # create response
@@ -123,3 +63,75 @@ class ResultDownloadView(BrowserView):
             if os.path.exists(fname):
                 os.remove(fname)
             raise e
+
+
+    def _download_results(self, context, zfile):
+        # FIXME: This is a rather lengthy process, and should probably be turned into a background task... (maybe as part of a datamanager service?)
+
+        # 1. find all IBlobDataset/ IRemotedDataset/ IDataset objects within context
+        pc = getToolByName(context, 'portal_catalog')
+        brains = pc.searchResults(path='/'.join(context.getPhysicalPath()),
+                                  object_provides=[IBlobDataset.__identifier__,
+                                                   IRemoteDataset.__identifier__])
+        metadata = {}
+
+        # the file/folder name for the zip
+        zfilename = context.title
+        # iterate over files and add to zip
+        for brain in brains:
+            content = brain.getObject()
+            if IBlobDataset.providedBy(content):
+                # If data is stored locally:
+                arcname = '/'.join((zfilename, 'data', content.file.filename))
+                # ob.file should be a NamedFile ... need to get fs name for that
+                blobfile = content.file.openDetached()
+
+                zfile.write(blobfile.name, arcname)
+                blobfile.close()
+
+            elif IRemoteDataset.providedBy(content):
+                # TODO: duplicate code from
+                remoteUrl = getattr(content, 'remoteUrl', None)
+                if remoteUrl is None:
+                    raise NotFound(self, 'remoteUrl', self.request)
+                # get arcname from remoteUrl
+                arcname = '/'.join((zfilename, 'data', os.path.basename(remoteUrl)))
+                # FIXME: should check dataset downloaiable flag here,
+                #       but assumption is, that this function can only be called on an experiment result folder....
+                # TODO: duplicate code in browser/dataset.py:RemoteDatasetDownload.__call__
+                # TODO: may not work in general... it always uses swift as remote url
+                tool = getUtility(ISwiftUtility)
+                try:
+                    url = tool.generate_temp_url(url=remoteUrl)
+                except:
+                    url = remoteUrl
+                # url is now the location from which we can fetch the file
+                temp_file, _ = urlretrieve(url)
+                zfile.write(temp_file, arcname)
+                os.remove(temp_file)
+            else:
+                # unknown type of Dataset
+                # just skip it
+                # TODO: Log warning or debug?
+                continue
+            metadata[arcname] = getdsmetadata(content)
+        # all files are in ....
+        # TODO: add experiment result metadata
+
+        # put metadata into zip
+        # provenance data stored on result container
+        provdata = IProvenanceData(context)
+        if not provdata.data is None:
+            zfile.writestr('/'.join((zfilename, 'prov.ttl')),
+                           provdata.data)
+
+        # add exp metadata
+        expmetadata = IExperimentMetadata(context)
+        if not expmetadata.data is None:
+            zfile.writestr('/'.join((zfilename, 'expmetadata.txt')),
+                           expmetadata.data)
+
+        # add mets.xml
+        metsview = getMultiAdapter((context, self.request), name="mets.xml")
+        zfile.writestr('/'.join((zfilename, 'mets.xml')),
+                       metsview.render())
