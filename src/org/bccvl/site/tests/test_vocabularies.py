e59903438ece59fdc33fd02ea6a1b22d83f4217e
Yong Liaw <y.liaw@griffith.edu.au>
2018-03-19 11:00:34 +1000

    Revert "BCCVL-168: Add new layer code for Freshwater climate datasets."
    This reverts commit d01a9c5862713b51c3f1c5dfdf320555ec79050a.

--- src/org/bccvl/site/tests/test_vocabularies.py
+++ src/org/bccvl/site/tests/test_vocabularies.py
@@ -25,7 +25,7 @@ class LayerSourceTest(unittest.TestCase):
         self.assertIn(u'B01', source)
         term = source.getTerm(u'B01')
         self.assertEqual(term.value, u'B01')
-        self.assertEqual(len(source), 446)
+        self.assertEqual(len(source), 334)
 
 
 class GCMSourceTest(unittest.TestCase):
