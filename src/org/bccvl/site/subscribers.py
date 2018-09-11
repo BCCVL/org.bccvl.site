e03dd7cb2ff5bc1fe78f0faeb872d6b3c2e6aace
James Li <j.li7@griffith.edu.au>
2018-09-11 15:23:48 +1000

    Ignore `came_from` and go straight to Dashboard regardless of originating page

--- src/org/bccvl/site/subscribers.py
+++ src/org/bccvl/site/subscribers.py
@@ -6,33 +6,18 @@ def logged_in_handler(event):
     """
     Listen to the event and perform the action accordingly.
     """
-    #user = event.object
-
-    # check for came_from or next url ....
-    # if empty go to dashboard otherwise to came_from
     request = getRequest()
     if request is None:
         # HTTP request is not present e.g.
         # when doing unit testing / calling scripts from command line
         return
 
-    came_from = request.get('came_from', None)
-    site_url = getSite().absolute_url()
-    if came_from:
-        if came_from.endswith('/'):
-            # get rid of trailing slash for comparison
-            came_from = came_from.rstrip('/')
-        if came_from != site_url:
-            # do nothing continue normal login redirect behaviour
-            return
-
-    # check if came_from is not empty, then clear it up, otherwise further
-    # Plone scripts will override our redirect
-    if came_from:
+    # Go straight to Dashboard regardless of the originating page
+    # As part of this, we must clear out the `came_from` parameter as other
+    # handlers in the chain may pick up on it and change our redirect
+    if request.get('came_from', None):
         request['came_from'] = ''
         request.form['came_from'] = ''
 
-    # no redirect set, so go to dashboard
+    site_url = getSite().absolute_url()
     request.RESPONSE.redirect('{0}/dashboard'.format(site_url))
-
-    #/login_next python script?
