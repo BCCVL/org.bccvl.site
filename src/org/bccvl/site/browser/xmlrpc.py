fc2af3ce51219f38584fdda513cdb167cd274272
Yong Liaw <y.liaw@griffith.edu.au>
2019-05-30 12:08:15 +1000

    Revert "BCCVL-335: Update to use taxon_concept_lsid for occurrence search endpoint."
    This reverts commit ae62df817b85088bd870485a1fcfa169a093ae10.

--- src/org/bccvl/site/browser/xmlrpc.py
+++ src/org/bccvl/site/browser/xmlrpc.py
@@ -145,7 +145,7 @@ class DataMover(BrowserView):
         species = set()
         if dataSrc == 'ala':
             params = [{
-                'query': 'taxon_concept_lsid:{}'.format(lsid),
+                'query': 'lsid:{}'.format(lsid),
                 'url': 'https://biocache-ws.ala.org.au/ws'
             }]
             for query in params:
